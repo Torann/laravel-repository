3613f2cf6f343772a154f18c9b6b181d90cd8434
Daniel Stainback <torann@gmail.com>
2015-12-15 20:43:39 -0500

    Listener was missing

---
+++ src/Torann/LaravelRepository/Listeners/CleanCacheRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Torann\LaravelRepository\Listeners;
+
+use Torann\LaravelRepository\Events\RepositoryEntityEvent;
+
+class CleanCacheRepository
+{
+    /**
+     * Clear cache for the given repository.
+     *
+     * @param RepositoryEntityEvent $event
+     */
+    public function handle(RepositoryEntityEvent $event)
+    {
+        $repository = $event->getRepository();
+        $action = $event->getAction();
+
+        if (in_array(config('cache.default'), ['file', 'database']) === false
+            && config("repositories.cache.clean.on.{$action}", true)
+        ) {
+            app('Illuminate\Cache\CacheManager')->tags($repository)->flush();
+        }
+    }
+}
